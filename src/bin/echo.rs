1ba3e3919e2cb303f7729f89d2312dfe7e4c152a
Emanuel Solis <esolis6114@gmail.com>
2024-09-29 00:06:20 -0400

    Move init logic into lib
    We extract the init message in lib rather than main's step function.
    This simplifies main's implementation and allows us to store the node_id
    string in our Node struct which we later use to generate unique strings.

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -1,4 +1,4 @@
-use anyhow::{self, bail, Context, Ok};
+use anyhow::{self, Context, Ok};
 use rustcurrent::*;
 use serde::{Deserialize, Serialize};
 use serde_json::{self};
@@ -8,43 +8,23 @@ use std::io::{StdoutLock, Write};
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
 enum Payload {
-    Echo {
-        echo: String,
-    },
-    EchoOk {
-        echo: String,
-    },
-    Init {
-        node_id: String,
-        node_ids: Vec<String>,
-    },
-    InitOk,
+    Echo { echo: String },
+    EchoOk { echo: String },
 }
 
 struct EchoNode {
     id: usize,
 }
 
-impl Node<Payload> for EchoNode {
+impl Node<(), Payload> for EchoNode {
+    fn from_init(_state: (), _init: rustcurrent::Init) -> anyhow::Result<Self>
+    where
+        Self: Sized,
+    {
+        Ok(EchoNode { id: 1 })
+    }
     fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()> {
         match input.body.payload {
-            Payload::Init { .. } => {
-                let reply = Message {
-                    src: input.dst,
-                    dst: input.src,
-                    body: Body {
-                        to: input.body.id,
-                        payload: Payload::InitOk,
-                        id: Some(self.id),
-                    },
-                };
-                serde_json::to_writer(&mut *output, &reply)
-                    .context("serialize response to Init")?;
-                output.write_all(b"\n").context("write trailing newline")?;
-
-                self.id += 1;
-            }
-            Payload::InitOk => bail!("Received init_ok message"),
             Payload::Echo { echo } => {
                 let reply = Message {
                     src: input.dst,
@@ -69,5 +49,5 @@ impl Node<Payload> for EchoNode {
 }
 
 fn main() -> anyhow::Result<()> {
-    main_loop(EchoNode { id: 0 })
+    main_loop::<_, EchoNode, _>(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use anyhow::{self, Context, Ok};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use serde_json::{self};
-use std::io::{stdin, stdout, StdoutLock};
+use std::io::{stdin, stdout, BufRead, StdoutLock, Write};
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
 #[serde(rename_all = "snake_case")]
@@ -24,30 +24,73 @@ pub struct Body<Payload> {
 }
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum InitPayload {
+    Init(Init),
+    InitOk,
+}
+
+#[derive(Clone, Serialize, Deserialize, Debug)]
 pub struct Init {
     pub node_id: String,
     pub node_ids: Vec<String>,
 }
 
-pub trait Node<Payload> {
+pub trait Node<S, Payload> {
+    fn from_init(state: S, init: Init) -> anyhow::Result<Self>
+    where
+        Self: Sized;
     fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()>;
 }
 
-pub fn main_loop<S, Payload>(mut state: S) -> anyhow::Result<()>
+/// Main loop now handles input message by reading the first message from stdin which is guaranteed
+/// to be an Init Message
+pub fn main_loop<S, N, P>(init_state: S) -> anyhow::Result<()>
 where
-    S: Node<Payload>,
-    Payload: DeserializeOwned,
+    P: DeserializeOwned,
+    N: Node<S, P>,
 {
-    let _stdin = stdin().lock();
-    let inputs = serde_json::Deserializer::from_reader(_stdin).into_iter::<Message<Payload>>();
-
+    let mut stdin = stdin().lock();
+    let mut stdin = stdin.lines();
     let mut _stdout = stdout().lock();
 
-    for input in inputs {
-        let input = input.context("Maelstrom input from STDIN could not be deserialized")?;
+    // We use stdin lines here bc we know that the format is newline separated
+    // A StreamDeserializer checks whether it can deserialize at the
+    // end of newlines which is more overhead that we can easily avoid
+    let init_msg: Message<InitPayload> = serde_json::from_str(
+        &stdin
+            .next()
+            .expect("no init message received")
+            .context("failed to read init message from stdin")?,
+    )
+    .context("init message could not be deserialized")?;
 
-        state
-            .step(input, &mut _stdout)
+    let InitPayload::Init(init) = init_msg.body.payload else {
+        panic!("first message should be init");
+    };
+    let mut node: N = Node::from_init(init_state, init).context("node initialization failed")?;
+
+    let reply = Message {
+        src: init_msg.dst,
+        dst: init_msg.src,
+        body: Body {
+            to: init_msg.body.id,
+            payload: InitPayload::InitOk,
+            // We're reserving 0 for the init_ok message
+            // Starting nodes with id = 1
+            id: Some(0),
+        },
+    };
+    serde_json::to_writer(&mut _stdout, &reply).context("serialize response to Init")?;
+    _stdout.write_all(b"\n").context("write trailing newline")?;
+
+    for line in stdin {
+        let line = line.context("Maelstrom input from STDIN could not be read")?;
+        let input: Message<P> = serde_json::from_str(&line)
+            .context("Maelstrom input from STDIN could not be deserialized")?;
+
+        node.step(input, &mut _stdout)
             .context("Node step function failed")?;
     }
 
